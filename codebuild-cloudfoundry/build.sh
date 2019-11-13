d1d6f30d7f665113317242743adc956e994e9e84
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2019-11-13 15:56:37 +0000

    Lawrence & Christine: move dockerfiles to alternative dir

--- codebuild-cloudfoundry/build.sh
+++ codebuild-cloudfoundry/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 git submodule init
-docker build -t aws-standard-ubuntu aws-codebuild-docker-images/ubuntu/standard/2.0
+docker build -t aws-standard-ubuntu aws-codebuild-docker/ubuntu/standard/2.0
 docker build -t codebuild-cloudfoundary ./
