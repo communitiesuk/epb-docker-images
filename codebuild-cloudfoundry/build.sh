93125b2c277a62e25b9c52ec3e20220dd136081d
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2019-11-13 09:22:59 +0000

    Lawrence: correction to folder name

--- codebuild-cloudfoundry/build.sh
+++ codebuild-cloudfoundry/build.sh
@@ -2,4 +2,4 @@
 
 git submodule init
 docker build -t aws-standard-ubuntu aws-codebuild-docker-images/ubuntu/standard/2.0
-docker build -t codebuild-cloudfoundary ./
+docker build -t codebuild-cloudfoundry ./
