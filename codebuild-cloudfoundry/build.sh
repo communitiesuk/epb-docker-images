557f269180f7b7240823cfae2fceb4bae9b2ac19
Stéphane Meny <smeny@users.noreply.github.com>
2021-07-09 11:37:22 +0100

    Upgrade codebuild image to 5.0

--- codebuild-cloudfoundry/build.sh
+++ codebuild-cloudfoundry/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t aws-standard-ubuntu aws-codebuild-docker/ubuntu/standard/2.0
-docker build -t codebuild-cloudfoundary ./
+docker build -t aws-standard-ubuntu aws-codebuild-docker/ubuntu/standard/5.0
+docker build -t codebuild-cloudfoundry ./
