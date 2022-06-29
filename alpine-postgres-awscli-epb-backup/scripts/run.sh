f12b47f0d218123515884d2170f6f4e7d157c7f9
Barry Halper <barryhalper@gmail.com>
2022-06-29 15:41:06 +0100

    Updated run script

--- alpine-postgres-awscli-epb-backup/scripts/run.sh
+++ alpine-postgres-awscli-epb-backup/scripts/run.sh
@@ -2,11 +2,12 @@
 
 set -eu
 
+now=$(date)
+
+echo "Date container stared is: $now"
 # check AWS credentials and bucket working
 S3_BASE_URI=s3://${S3_BUCKET:?required}
 
 echo "Testing AWS credentials to access ${S3_BASE_URI}"
 aws s3 ls "${S3_BASE_URI}/"
 
-
-datestamp=$(curl -sL http://dilbert.com/ | pup "h1.comic-title a attr{href}" | head -n1 | sed -e "s%^.*/%%")
