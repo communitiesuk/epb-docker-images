2259f7cce8284d8b0d5ae909db0ab91bb50c215b
Barry Halper <barryhalper@gmail.com>
2022-07-05 09:42:13 +0100

    Updated run script to push yml to s3 using mv

--- alpine-postgres-awscli-epb-backup/scripts/run.sh
+++ alpine-postgres-awscli-epb-backup/scripts/run.sh
@@ -6,5 +6,5 @@ set -eu
 S3_BASE_URI=s3://${BUCKET_NAME:?required}
 
 echo ">>>> UPLOADING FILE S3"
-aws s3 rm buildspec.yml "${S3_BASE_URI}/buildspec.yml"
+aws s3 mv buildspec.yml "${S3_BASE_URI}/buildspec.yml"
 
